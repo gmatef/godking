dd5ffe18a4c24cbe71c86ace1411685066d6d390
Gastòn Fernandez <gastonmfdz@gmal.com>
2022-12-31 00:42:33 -0300

    Integracion con bootstrap

--- ui/src/app/app.component.ts
+++ ui/src/app/app.component.ts
@@ -1,4 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
+
+interface Alert {
+  type: string;
+  message: string;
+}
+
+const ALERTS: Alert[] = [
+  {
+    type: 'success',
+    message: 'This is an success alert',
+  },
+  {
+    type: 'info',
+    message: 'This is an info alert',
+  },
+  {
+    type: 'warning',
+    message: 'This is a warning alert',
+  },
+  {
+    type: 'danger',
+    message: 'This is a danger alert',
+  },
+  {
+    type: 'primary',
+    message: 'This is a primary alert',
+  },
+  {
+    type: 'secondary',
+    message: 'This is a secondary alert',
+  },
+  {
+    type: 'light',
+    message: 'This is a light alert',
+  },
+  {
+    type: 'dark',
+    message: 'This is a dark alert',
+  },
+];
 
 @Component({
   selector: 'app-root',
@@ -7,4 +48,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ui';
+  alerts: Alert[] = [];
+  /* @ViewChild('a') alert!: NgbAlert */
+  visible: boolean = true
+
+  constructor() {
+    this.reset();
+  }
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
+  reset() {
+    this.alerts = Array.from(ALERTS);
+  }
+
+  /* alert.close().subscribe(rep: any => {}: any); */
 }
